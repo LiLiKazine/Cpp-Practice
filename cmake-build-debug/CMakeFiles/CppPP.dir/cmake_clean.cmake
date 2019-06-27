76e772758a08c3aeea930890aed8bdd04e5fe47b
LiLi Kazine <lilikazine@gmail.com>
2019-06-27 16:17:29 +0800

    initial

---
+++ cmake-build-debug/CMakeFiles/CppPP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppPP.dir/cp12/string1.cpp.o"
+  "CMakeFiles/CppPP.dir/test.cpp.o"
+  "CMakeFiles/CppPP.dir/cp12/sayings1.cpp.o"
+  "CppPP.pdb"
+  "CppPP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Li Sheng on 2019-06-27.
+//
+
+#include "test.h"
