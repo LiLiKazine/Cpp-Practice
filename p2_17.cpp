805f1b33b80bf8e9f38b6f717144a0d2f9ee1ebf
LiLi Kazine <lilikazine@gmail.com>
2020-03-21 18:08:12 +0800

    p2_17

---
+++ p2_17.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout<<i+1<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
--- test.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Li Sheng on 2019-06-27.
-//
-
-#include "test.h"
