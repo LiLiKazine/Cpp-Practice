7f2a0069b1b090ab31e1541067b66a75acb4b2d6
LiLi Kazine <lilikazine@gmail.com>
2020-03-21 18:04:27 +0800

    p2_16

---
+++ p2_16.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    int a, b;
+    cin>>a>>b;
+    cout<<"sum: "<<a+b<<endl;
+    cout<<"multiply: "<<a*b<<endl;
+    cout<<"diff: "<<a-b<<endl;
+    cout<<"divide: "<<a/b<<endl;
+
+    return 0;
+}
