7cebe80ede8d485020709e6fd63f8c40d1da95ca
LiLi Kazine <lilikazine@gmail.com>
2020-03-23 13:50:15 +0800

    p2_20

---
+++ p2_20.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int r;
+    cin>>r;
+    cout<<"diameter: "<<r*2<<endl;
+    cout<<"perimeter: "<<3.14159*r*2<<endl;
+    cout<<"area: "<<3.14159*r*r<<endl;
+    return 0;
+}
